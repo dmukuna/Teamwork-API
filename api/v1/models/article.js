f3659cfa25de4e4536ac9e6d6f2576294f4be676
dmukuna <danielmukuna.k@outlook.com>
2019-11-22 05:57:16 +0300

    [finished] writing article model

---
+++ api/v1/models/article.js
@@ -0,0 +1,69 @@
+import db from './index';
+
+const { query } = db;
+
+const Article = {
+  findOneArticle(value) {
+    const queryText = 'SELECT * FROM articles WHERE ID=$1';
+    const row = query(queryText, value)
+      .then((res) => res.rows[0])
+      .catch((err) => {
+        throw err;
+      });
+    return row;
+  },
+
+  findAllArticles() {
+    const queryText = 'SELECT * FROM articles';
+    const rows = query(queryText, [])
+      .then((res) => res.rows)
+      .catch((err) => {
+        throw err;
+      });
+    return rows;
+  },
+
+  updateArticle(values) {
+    const queryText = 'UPDATE articles SET article=$1 WHERE ID=$2 RETURNING *';
+    const row = query(queryText, values)
+      .then((res) => res.rows[0])
+      .catch((err) => {
+        throw err;
+      });
+    return row;
+  },
+
+  saveArticle(values) {
+    const queryText = `INSERT INTO
+      articles (ID, title, article, createdOn, authorId)
+      VALUES ($1, $2, $3, $4, $5)`;
+    const row = query(queryText, values)
+      .then((res) => res.rows[0])
+      .catch((err) => {
+        throw err;
+      });
+    return row;
+  },
+
+  deleteArticle(value) {
+    const queryText = 'DELETE FROM articles WHERE ID=$1 RETURNING *';
+    const row = query(queryText, value)
+      .then((res) => res.rows[0])
+      .catch((err) => {
+        throw err;
+      });
+    return row;
+  },
+
+  deleteAllArticles() {
+    const queryText = 'DELETE FROM articles';
+    const rows = query(queryText, [])
+      .then((res) => res.rows)
+      .catch((err) => {
+        throw err;
+      });
+    return rows;
+  },
+};
+
+export default Article;
