19c136383bb530e5dd87af5cc46102a16111a35b
dmukuna <danielmukuna.k@outlook.com>
2019-11-22 06:36:08 +0300

    [finished] creating the comment model

---
+++ api/v1/models/comment.js
@@ -0,0 +1,91 @@
+import db from './index';
+
+const { query } = db;
+
+const Comment = {
+  findOneComment(value) {
+    const queryText = 'SELECT * FROM comments WHERE ID=$1';
+    const row = query(queryText, value)
+      .then((res) => res.rows[0])
+      .catch((err) => {
+        throw err;
+      });
+    return row;
+  },
+
+  findAllComments() {
+    const queryText = 'SELECT * FROM comments';
+    const rows = query(queryText, [])
+      .then((res) => res.rows)
+      .catch((err) => {
+        throw err;
+      });
+    return rows;
+  },
+
+  findArticleComments(value) {
+    const queryText = 'SELECT * FROM comments WHERE articleId=$1';
+    const rows = query(queryText, value)
+      .then((res) => res.rows)
+      .catch((err) => {
+        throw err;
+      });
+    return rows;
+  },
+
+  findGifComments(value) {
+    const queryText = 'SELECT * FROM comments WHERE gifId=$1';
+    const rows = query(queryText, value)
+      .then((res) => res.rows)
+      .catch((err) => {
+        throw err;
+      });
+    return rows;
+  },
+
+  saveGifComment(values) {
+    const queryText = `INSERT INTO
+      comments (ID, comment, createdOn, authorId, gifId)
+      VALUES ($1, $2, $3, $4, $5)`;
+    const row = query(queryText, values)
+      .then((res) => res.rows[0])
+      .catch((err) => {
+        throw err;
+      });
+    return row;
+  },
+
+  saveArticleComment(values) {
+    const queryText = `INSERT INTO
+      comments (ID, comment, createdOn, authorId, articleId)
+      VALUES ($1, $2, $3, $4, $5)`;
+    const row = query(queryText, values)
+      .then((res) => res.rows[0])
+      .catch((err) => {
+        throw err;
+      });
+    return row;
+  },
+
+  deleteComment(value) {
+    const queryText = 'DELETE FROM comments WHERE ID=$1 RETURNING *';
+    const row = query(queryText, value)
+      .then((res) => res.rows[0])
+      .catch((err) => {
+        throw err;
+      });
+    return row;
+  },
+
+  deleteAllComments() {
+    const queryText = 'DELETE FROM comments RETURNING *';
+    const rows = query(queryText, [])
+      .then((res) => res.rows)
+      .catch((err) => {
+        throw err;
+      });
+    return rows;
+  },
+};
+
+export default Comment;
