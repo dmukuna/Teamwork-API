1bca45a3e5111c52283691adad78d278a0e41d12
dmukuna <danielmukuna.k@outlook.com>
2019-11-22 06:59:05 +0300

    [finished] setting up role-based authorization

--- api/v1/app.js
+++ api/v1/app.js
@@ -2,6 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+import gifRoutes from './routes/gif';
+import articleRoutes from './routes/article';
+import userRoutes from './routes/user';
+import feedRoutes from './routes/feed';
+import auth from './middleware/auth';
+import Role from './config/rolesConfig';
+
 
 const app = express();
 
@@ -10,5 +17,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+
+app.use('/api/v1/auth', userRoutes);
+app.use('/api/v1/articles', auth([Role.Employee, Role.Admin]), articleRoutes);
+app.use('/api/v1/gifs', auth([Role.Employee, Role.Admin]), gifRoutes);
+app.use('/api/v1/feed', auth([Role.Employee, Role.Admin]), feedRoutes);
 
 module.exports = app;
