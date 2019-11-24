40866e210df00174055f09d6a0f659689b98e6ae
dmukuna <danielmukuna.k@outlook.com>
2019-11-24 19:39:00 +0300

    [finished] configuring testing database

--- api/v1/config/dbConfig.js
+++ api/v1/config/dbConfig.js
@@ -6,18 +6,25 @@ dotenv.config();
 const isProduction = process.env.NODE_ENV === 'production';
 const isDevelopment = process.env.NODE_ENV === 'development';
 let conn;
+let c;
 
 if (isProduction) {
-  conn = process.env.DATABASE_URL;
+  conn = {connectionString :process.env.DATABASE_URL};
+
 } else if (isDevelopment) {
-  conn = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+  conn = {connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`};
 } else {
-  conn = process.env.DATABASE_URL_TEST;
+  conn = {  
+    user: process.env.DB_USER_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_DATABASE_TEST,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    ssl: true
+  }
 }
 
-const pool = new Pool({
-  connectionString: conn,
-});
+const pool = new Pool(conn);
 
 pool.on('error', (err, client) => {
   client.release();
