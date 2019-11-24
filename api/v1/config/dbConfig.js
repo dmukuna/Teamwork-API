813deaa598763e4a299a364c3f53ba364e10f517
dmukuna <danielmukuna.k@outlook.com>
2019-11-24 19:45:15 +0300

    [finished] configuring testing database

--- api/v1/config/dbConfig.js
+++ api/v1/config/dbConfig.js
@@ -18,8 +18,8 @@ if (isProduction) {
     user: process.env.DB_USER_TEST,
     password: process.env.DB_PASSWORD_TEST,
     database: process.env.DB_DATABASE_TEST,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
+    port: process.env.DB_PORT_TEST,
+    host: process.env.DB_HOST_TEST,
     ssl: true
   }
 }
